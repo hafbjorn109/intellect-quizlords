11c6a859a6be53e035f754095ff3279d1f1fd42f
Kamil Giszka <kamilgiszka109@gmail.com>
2025-06-11 21:09:11 +0200

    question and answer models added

--- quiz/models.py
+++ quiz/models.py
@@ -8,3 +8,28 @@ class Category(db.Model):
 
     def __repr__(self):
         return f'<Category "{self.name}">'
+
+
+class Question(db.Model):
+    __tablename__ = 'questions'
+
+    id = db.Column(db.Integer, primary_key=True)
+    text = db.Column(db.String(255), nullable=False)
+    category_id = db.Column(db.Integer, db.ForeignKey('categories.id'), nullable=False)
+    category = db.relationship(Category, back_populates="questions")
+    answers = db.relationship('Answer', back_populates='question', cascade="all, delete")
+
+    def __repr__(self):
+        return f'<Question "{self.text}">'
+
+
+class Answer(db.Model):
+    __tablename__ = 'answers'
+    id = db.Column(db.Integer, primary_key=True)
+    text = db.Column(db.String(255), nullable=False)
+    is_correct = db.Column(db.Boolean, default=False, nullable=False)
+    question_id = db.Column(db.Integer, db.ForeignKey('questions.id'), nullable=False)
+    question = db.relationship(Question, back_populates="answers")
+
+    def __repr__(self):
+        return f'<Answer {self.text } (correct={self.is_correct})>'
