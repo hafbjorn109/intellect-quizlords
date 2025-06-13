f64a0f7d474f6e2472399af5cddeea88cc0f093d
Kamil Giszka <kamilgiszka109@gmail.com>
2025-06-13 17:53:05 +0200

    crud for categories done

--- quiz/routes/categories.py
+++ quiz/routes/categories.py
@@ -23,3 +23,37 @@ def create_category():
     db.session.commit()
 
     return jsonify(category_schema.dump(category)), 201
+
+
+@bp.route('/', methods=['GET'])
+def get_categories():
+    categories = Category.query.all()
+    return jsonify(categories_schema.dump(categories)), 200
+
+
+@bp.route('/<int:category_id>', methods=['GET'])
+def get_category(category_id):
+    category = Category.query.get_or_404(category_id)
+    return jsonify(category_schema.dump(category)), 200
+
+
+@bp.route('/<int:category_id>', methods=['PUT'])
+def update_category(category_id):
+    category = Category.query.get_or_404(category_id)
+    data = request.get_json()
+
+    errors = category_schema.validate(data)
+    if errors:
+        return jsonify({'errors': errors}), 400
+
+    category.name = data['name']
+    db.session.commit()
+    return jsonify(category_schema.dump(category)), 200
+
+
+@bp.route('/<int:category_id>', methods=['DELETE'])
+def delete_category(category_id):
+    category = Category.query.get_or_404(category_id)
+    db.session.delete(category)
+    db.session.commit()
+    return jsonify({'message': 'Category deleted'}), 200
