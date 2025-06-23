8c6bff7ee090862113acef890105fad41d8480f2
Kamil Giszka <kamilgiszka109@gmail.com>
2025-06-23 19:23:29 +0200

    GameSession and Player models added

--- quiz/models.py
+++ quiz/models.py
@@ -1,3 +1,4 @@
+import string, random
 from db import db
 
 class Category(db.Model):
@@ -26,6 +27,7 @@ class Question(db.Model):
 
 class Answer(db.Model):
     __tablename__ = 'answers'
+    
     id = db.Column(db.Integer, primary_key=True)
     text = db.Column(db.String(255), nullable=False)
     is_correct = db.Column(db.Boolean, default=False, nullable=False)
@@ -34,3 +36,26 @@ class Answer(db.Model):
 
     def __repr__(self):
         return f'<Answer {self.text } (correct={self.is_correct})>'
+
+class GameSession(db.Model):
+    __tablename__ = 'game_sessions'
+
+    id = db.Column(db.Integer, primary_key=True)
+    code = db.Column(db.String(8), nullable=False, unique=True)
+    is_active = db.Column(db.Boolean, default=True, nullable=False)
+    players = db.relationship('Player', back_populates='session', cascade="all, delete")
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        if not self.code:
+            self.code = ''.join(random.choices(string.ascii_uppercase + string.digits, k=8))
+
+
+class Player(db.Model):
+    __tablename__ = 'players'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(25), nullable=False)
+    is_ready = db.Column(db.Boolean, default=False, nullable=False)
+    session_id = db.Column(db.Integer, db.ForeignKey('game_sessions.id'), nullable=False)
+    session = db.relationship(GameSession, back_populates='players')
